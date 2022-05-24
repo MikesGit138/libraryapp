983703051b77720a564f7c9cd4e2c013bc183056
Michael <michael138leighton@gmail.com>
2022-05-24 09:51:56 -0500

    server added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const port = 9000
+const bodyParser = require('body-parser');
+// const db = require('./database/db')
+const path = require('path');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//port listening
+app.listen(port, ()=> { console.log(`Server running on http://localhost:${port}/`)})
